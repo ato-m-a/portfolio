90f502088c2a6a2e714d02f5c7352685278c616a
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-19 01:55:02 +0900

    add robots, sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,21 @@
+import { FixtureRepository } from '@/common/repository';
+import type { Project } from '@/types/common/fixtures';
+import type { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const projects = FixtureRepository.get<Project>('project');
+  const filtered = projects.filter((project) => project.path);
+
+  return [
+    {
+      url: 'https://ato-m-a.me',
+      lastModified: new Date(),
+      priority: 1
+    },
+    ...filtered.map((project) => ({
+      url: `https://ato-m-a.me${project.path}`,
+      lastModified: new Date(),
+      priority: 0.8
+    }))
+  ]
+}

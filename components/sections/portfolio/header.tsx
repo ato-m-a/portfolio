e7275703085cb41a69ef173f39347acd75b5a415
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-19 08:42:48 +0900

    blur 이미지 추가

--- components/sections/portfolio/header.tsx
+++ components/sections/portfolio/header.tsx
@@ -2,12 +2,16 @@
 
 import { useAnimation } from '@/hooks/useAnimation';
 import Image from 'next/image';
-import { useRef, type ReactElement } from 'react';
+import { useMemo, useRef, type ReactElement } from 'react';
 import { HeaderProps } from './types/header';
 
 const Header = ({ src, title }: HeaderProps): ReactElement => {
   const imageWrapperRef = useRef<HTMLDivElement>(null);
   const headingRef = useRef<HTMLHeadingElement>(null);
+
+  const blurred = useMemo(() => {
+    return src.replace('.webp', '-blur.webp');
+  }, []);
 
   useAnimation(imageWrapperRef, true);
   useAnimation(headingRef, true);
@@ -15,7 +19,16 @@ const Header = ({ src, title }: HeaderProps): ReactElement => {
   return (
     <>
       <div className="relative w-full h-1/4 overflow-hidden" ref={imageWrapperRef}>
-        <Image src={src} alt="header image" className="object-cover w-full h-full" width={2000} height={2000} quality={100} loading="eager" />
+        <Image 
+          src={src} 
+          alt="header image" 
+          className="object-cover w-full h-full" 
+          placeholder="blur"
+          blurDataURL={blurred}
+          width={2000} 
+          height={2000} 
+          quality={100} 
+        />
       </div>
       <h1 className="notion-page inline-block font-pretendard font-semibold text-4xl !pt-10 !pb-2 max-md:text-3xl" ref={headingRef}>{title}</h1>
     </>

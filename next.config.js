884c292cea68817d809bfe9e93e502909b78ce75
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-20 04:38:54 +0900

    CSP

--- next.config.js
+++ next.config.js
@@ -1,11 +1,60 @@
 const path = require('path');
 
+const ConentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com www.google-analytics.com;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  connect-src *;
+  font-src 'self';
+  frame-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ConentSecurityPolicy.replace(/\n/g, '')
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload'
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()'
+  }
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { webpack }) => {
     config.resolve.alias['@'] = path.resolve(__dirname, '.');
 
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders
+      }
+    ]
   }
 }
 

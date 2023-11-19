b203cab340b731cce43eb3924285f94e0b5cbad2
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-20 01:27:19 +0900

    div 가 p의 자손이 되어 수정

--- components/layout/section/subtitle.tsx
+++ components/layout/section/subtitle.tsx
@@ -1,7 +1,7 @@
 import type { ReactFC } from '@/types/components/fc';
 
 const Subtitle: ReactFC = ({ children, className }) => {
-  return <p className={`text-xl text-center font-pretendard ${className ?? ''}`}>{children}</p>;
+  return <div className={`text-xl text-center font-pretendard ${className ?? ''}`}>{children}</div>;
 }
 
 export default Subtitle;

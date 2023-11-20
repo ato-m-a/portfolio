7d19160ab80f65d32e40751662cd9cc3fa05badc
Tom <99224451+ato-m-a@users.noreply.github.com>
2023-11-20 14:07:17 +0900

    @/hooks/useAnimation.ts 수정
    once = true일 때 일방적으로 observe하도록 변경

--- hooks/useAnimation.ts
+++ hooks/useAnimation.ts
@@ -3,29 +3,34 @@ import { selectObserver } from '@/store/observer';
 import { useEffect, type RefObject } from 'react';
 import { useRecoilValue } from 'recoil';
 
+/**
+ * @param ref observe 할 element
+ * @param once 한 번만, 일방적으로(only animateIn) observe할지 여부
+ */
 export const useAnimation = (ref: RefObject<HTMLElement>, once: boolean = false) => {
   const observers = useRecoilValue(selectObserver);
+  const element = ref.current;
 
   useEffect(() => {
-    const element = ref.current;
-
     if (element && observers) {
       observers.forEach(observer => observer.observe(element));
 
       if (once) {
+        observers[0].observe(element);
+
         // animation의 duration 만큼의 시간이 지나면 unobserve
-        const unobserveTimer = setTimeout(() => {
-          observers.forEach(observer => observer.unobserve(element));
-        }, duration);
+        const unobserveTimer = setTimeout(() => observers[0].unobserve(element), duration);
 
         return () => {
           clearTimeout(unobserveTimer);
         }
+      } else {
+        observers.forEach(observer => observer.observe(element));
+
+        return () => {
+          observers.forEach(observer => observer.unobserve(element));
+        }
       }
     }
-
-    return () => {
-      if (element && observers) observers.forEach(observer => observer.unobserve(element));
-    };
   }, []);
 }

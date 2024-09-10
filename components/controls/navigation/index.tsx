6fc707686d5d51f9cfacabf7297075daca80402b
Tom <99224451+ato-m-a@users.noreply.github.com>
2024-09-10 11:15:55 +0900

    feat: navigation elem 버튼으로 변경

--- components/controls/navigation/index.tsx
+++ components/controls/navigation/index.tsx
@@ -20,10 +20,10 @@ const Navigation = (): ReactElement => {
   return (
     <div className="fixed p-2 bottom-4 right-6 max-md:right-4">
       <div className="remote-wrapper max-md:px-2 max-md:py-2">
-        <p className="remote-button mb-3 max-md:w-8 max-md:h-8 max-md:text-lg" role="button" onClick={() => goTo('about')}>{Emoji.hello}</p>
-        <p className="remote-button mb-3 max-md:w-8 max-md:h-8 max-md:text-lg" role="button" onClick={() => goTo('skill')}>{Emoji.star}</p>
-        <p className="remote-button mb-3 max-md:w-8 max-md:h-8 max-md:text-lg" role="button" onClick={() => goTo('experience')}>{Emoji.briefcase}</p>
-        <p className="remote-button max-md:w-8 max-md:h-8 max-md:text-lg" role="button" onClick={() => goTo('project')}>{Emoji.rocket}</p>
+        <button type="button" className="remote-button mb-3 max-md:w-8 max-md:h-8 max-md:text-lg" onClick={() => goTo('about')}>{Emoji.hello}</button>
+        <button type="button" className="remote-button mb-3 max-md:w-8 max-md:h-8 max-md:text-lg" onClick={() => goTo('skill')}>{Emoji.star}</button>
+        <button type="button" className="remote-button mb-3 max-md:w-8 max-md:h-8 max-md:text-lg" onClick={() => goTo('experience')}>{Emoji.briefcase}</button>
+        <button type="button" className="remote-button max-md:w-8 max-md:h-8 max-md:text-lg" onClick={() => goTo('project')}>{Emoji.rocket}</button>
       </div>
     </div>
   )

d27eaa928cf987d20cfceb58477159134497c730
Tom <99224451+ato-m-a@users.noreply.github.com>
2024-09-10 17:59:21 +0900

    fix: notion page 색인시 구조분해 할당 제거

--- common/getNotionPage.ts
+++ common/getNotionPage.ts
@@ -3,12 +3,12 @@ import portfolioRepository from '@/repository/portfolio.repository';
 import { notFound } from 'next/navigation';
 
 const getNotionPage = async (pathname: string) => {
-  const { pageId, ...portfolio } = await portfolioRepository.get(pathname);
+  const portfolio = await portfolioRepository.get(pathname);
 
-  if (!pageId) return notFound();
+  if (!portfolio) return notFound();
 
   const notionClient = new NotionAPI();
-  const recordMap = await notionClient.getPage(pageId);
+  const recordMap = await notionClient.getPage(portfolio.pageId);
 
   return { portfolio, recordMap };
 }

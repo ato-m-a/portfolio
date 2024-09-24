f6cae8fbad78af3b4d36b00d3d11510afa5e4c83
Tom <atomjh0295@gmail.com>
2024-09-24 12:23:28 +0900

    fix: styling icon 수정

--- components/articles/skill/listItem/title/icon.tsx
+++ components/articles/skill/listItem/title/icon.tsx
@@ -7,7 +7,7 @@ type IconProps = Pick<SkillSchema, 'icon'>;
 const SkillIcon: FC<IconProps> = ({ icon }) => {
   const isAssetPathRegexp = new RegExp(/^\/images\/.*\.webp$/);
 
-  if (isAssetPathRegexp.test(icon)) return <Image src={icon} alt="skill logo" className="w-6 h-6 rounded" width={500} height={500} />
+  if (isAssetPathRegexp.test(icon)) return <Image src={icon} alt="skill logo" className="w-6 aspect-square rounded" width={500} height={500} />
   return <span className="text-xl">{icon}</span>
 }
 

e99e56e9c20c50b255a680b559489dd9483be72d
Tom <atomjh0295@gmail.com>
2024-09-26 15:00:01 +0900

    refactor: lastUpdatedAt 훅으로 리팩터링

--- components/misc/lastUpdated.tsx
+++ components/misc/lastUpdated.tsx
@@ -1,23 +1,14 @@
 "use client";
 
 import type { FC } from 'react';
-import getLastUpdatedAt from '@/common/getLastUpdatedAt';
-import trpc from '@trpc.client';
+import useLastUpdatedAt from '@/hooks/useLastUpdatedAt';
 
 const LastUpdated: FC = () => {
-  const data = trpc.useQueries((t) => [
-    t.skill.getMany(),
-    t.career.getMany(),
-    t.project.getMany(),
-  ]);
-
-  if (!data) return null;
-
-  const lastUpdated = getLastUpdatedAt(data);
+  const lastUpdatedAt = useLastUpdatedAt();
 
   return (
     <div className="fixed font-thin p-2 bottom-2 left-2 max-md:text-sm">
-      <p>{lastUpdated}</p>
+      <p>{lastUpdatedAt}</p>
     </div>
   )
 }
---
+++ hooks/useLastUpdatedAt.ts
@@ -0,0 +1,18 @@
+"use client";
+
+import trpc from '@trpc.client';
+import getLastUpdatedAt from '@/common/getLastUpdatedAt';
+
+const useLastUpdatedAt = () => {
+  const queries = trpc.useQueries((t) => [
+    t.skill.getMany(),
+    t.career.getMany(),
+    t.project.getMany(),
+  ]);
+
+  if (!queries) return null;
+
+  return getLastUpdatedAt(queries);
+}
+
+export default useLastUpdatedAt;

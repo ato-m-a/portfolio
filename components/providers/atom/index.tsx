faf2fa83f356e1e9a37f6ae5c71644c30178d1f8
Tom <atomjh0295@gmail.com>
2024-10-11 13:50:06 +0900

    chore: lint 설정

--- components/providers/atom/index.tsx
+++ components/providers/atom/index.tsx
@@ -1,14 +1,10 @@
-"use client";
+'use client';
 
 import type { ReactFC } from '@/types/components/fc';
 import { Provider } from 'jotai';
 
 const AtomProvider: ReactFC = ({ children }) => {
-  return (
-    <Provider>
-      {children}
-    </Provider>
-  )
-}
+  return <Provider>{children}</Provider>;
+};
 
 export default AtomProvider;
